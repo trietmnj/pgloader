73ac034125d5b26b16280451283dba9549c45848
TMNT <trietpersonal@gmail.com>
2022-07-10 13:21:04 -0400

    fixed USING clause

--- src/sources/mysql/sql/list-all-columns.sql
+++ src/sources/mysql/sql/list-all-columns.sql
@@ -11,7 +11,8 @@
          c.data_type, c.column_type, c.column_default,
          c.is_nullable, c.extra
     from information_schema.columns c
-         join information_schema.tables t using(table_schema, table_name)
+         --join information_schema.tables t using(table_schema, table_name)
+         join information_schema.tables t on c.table_schema=t.table_schema and c.table_name=t.table_name
    where c.table_schema = '~a' and t.table_type = '~a'
          ~:[~*~;and (~{table_name ~a~^ or ~})~]
          ~:[~*~;and (~{table_name ~a~^ and ~})~]
